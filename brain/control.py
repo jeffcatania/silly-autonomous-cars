9f7a52f9a504bf1697eb9e9dc278db343be5abb4
root <root@hackday-2.us-west1-b.c.science-hackday-project.internal>
2018-10-28 21:53:57 +0000

    moved server to brain

---
+++ brain/control.py
@@ -0,0 +1,39 @@
+import time
+import pyparticle as pp
+import os
+
+class Control:
+    particle = pp.Particle(access_token=os.getenv('PARTICLE_ACCESS_TOKEN', ''))
+    lock = False 
+
+    def process_start(self):
+       pass
+#       res = self.particle.call_function('3d0024000c47353136383631', 'power', 'on')
+
+    def process_stop(self):
+       pass
+#       res = self.particle.call_function('3d0024000c47353136383631', 'power', 'off')
+	
+
+    def process_request(self, req):
+        print(self.lock)
+        if self.lock:
+            return {
+                'status': 'failure',
+		'request': req
+            }
+        else:
+            #self.lock = True
+            res = {}
+            if req['state'] == "true":
+               try:
+		   ## TODO: strip device id and grab the req.device_id variable. requires change to iphone client.
+                   res = self.particle.call_function('3d0024000c47353136383631', 'dir', 'right')
+               except Exception as e:
+                   print(e)
+            self.lock = False
+            return {
+                'status': 'success',
+		'request': req,
+                'response': res
+            }
---
+++ brain/server.py
@@ -0,0 +1,40 @@
+import json
+import os
+from flask import Flask, request
+
+from brain.control import Control
+
+c = Control()
+
+app = Flask(__name__)
+access_token = os.getenv('PARTICLE_ACCESS_TOKEN', '')
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/api/start', methods=['GET'])
+def start():
+    r = c.process_start()
+    return json.dumps({
+        'response': r
+        }, indent=2)
+
+@app.route('/api/stop', methods=['GET'])
+def stop():
+    r = c.process_stop()
+    return json.dumps({
+        'response': r
+        }, indent=2)
+
+@app.route('/api/camera', methods=['GET'])
+def camera():
+    device_id = request.args.get('device_id')
+    state = request.args.get('state')
+    r = c.process_request({
+	'device_id': device_id,
+	'state': state
+    })
+    return json.dumps({
+        'response': r
+        }, indent=2)
